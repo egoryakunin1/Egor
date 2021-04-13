8b174c167c8d5e57d0f34b80121c08dc327ef750
egor <egor.yakunin.03@list.ru>
2021-04-13 11:38:22 +0700

    лог

---
+++ лог уроввнение.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("видите 1 число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("видите 2 число");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+            Console.ReadKey();
+            Console.WriteLine("видите 1 число");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("видите 2 число");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("видите 3 число");
+            double z = Convert.ToDouble(Console.ReadLine());
+            if (x < (y + z) & (y < (x + z)) & (z < (x + y)))
+            {
+                Console.WriteLine("треугольник есть");
+              
+            }
+            else
+            {
+                Console.WriteLine("треугольник не есть");
+            }
+            Console.ReadKey();
+
+        
+
+
+
+        }
+    }
+}
---
+++ матем виде инфы.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ведите чесло чесло х1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ведите чесло чесло х2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double x1x2 = (Math.Pow(x1, 2) + Math.Pow(x2, 2));
+            Console.WriteLine("ответ:" + x1x2);
+            Console.ReadKey();
+
+            Console.WriteLine("видите число y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("видите число y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("видите число y3");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            double y1y2y3 = y1 * y2 + y1 * y3 +y2 * y3;
+            Console.WriteLine("ответ" + y1y2y3);
+            Console.ReadKey();
+
+            Console.WriteLine("ведите чесло чесло z1");
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ведите чесло чесло a1");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ведите чесло чесло b1");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ведите чесло чесло d1");
+            double d1 = Convert.ToDouble(Console.ReadLine());
+            double l1 = a1 * b1 + (d1 * Math.Pow(a1, 2) / 2);
+            Console.WriteLine("ответ" + l1);
+            Console.ReadKey();
+
+
+        }
+    }
+}
